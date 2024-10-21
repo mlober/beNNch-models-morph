83b9a7ac032f683062e5a8bafb987fb422b9ea23
Melissa Lober <47082241+mlober@users.noreply.github.com>
2024-10-21 15:20:29 +0200

    Update parameter_space.py

--- multi-area-model/parameter_space.py
+++ multi-area-model/parameter_space.py
@@ -61,7 +61,7 @@ def calc_dependend_parameters(p):
     p['original']['withinarea']['N_total'] = int(p['area_size'] * p['scale'])
     p['original']['withinarea']['Nrec'] = int(min(p['original']['withinarea']['N_total'], 10000))         # number of neurons to record spikes from
     #p['original']['withinarea']['indegree'] = int(0.05 * p['original']['withinarea']['N_total'])         # total num connections per neuron
-    p['original']['withinarea']['indegree'] = 2921 # half of mean indegree of macaque multi-area model (half of incoming connections from within area, other half from other areas)
+    p['original']['withinarea']['indegree'] = int(2921*p['scale']) # half of mean indegree of macaque multi-area model (half of incoming connections from within area, other half from other areas)
   
     p['original']['interareal'] = {}
     if p['num_areas'] > 1:
